6087021050e421445cdb7f92157283f541954f95
Michelle Scott <scottmm374@gmail.com>
2019-12-18 16:58:15 -0500

    First get call done and working

---
+++ data/accounts/accountsRouter.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const db = require("../../data/dbConfig.js");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(await db("accounts").select());
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
