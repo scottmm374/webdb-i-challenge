b02ebbac84612c78f21c8142bb9d7935bc7e31af
Michelle Scott <scottmm374@gmail.com>
2019-12-18 19:59:59 -0500

    Did stretch and added a couple more queries for Get accounts

--- data/accounts/accountsRouter.js
+++ data/accounts/accountsRouter.js
@@ -2,14 +2,16 @@ const express = require("express");
 const db = require("../../data/dbConfig.js");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    res.json(await db("accounts").select());
-  } catch (err) {
-    next(err);
-  }
-});
+// GET ALL ACCOUNTS
+// router.get("/", async (req, res, next) => {
+//   try {
+//     res.json(await db("accounts").select());
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
+// GET ACCOUNT BY ID
 router.get("/:id", validateAccountsId, async (req, res, next) => {
   try {
     res.json(
@@ -22,6 +24,7 @@ router.get("/:id", validateAccountsId, async (req, res, next) => {
   }
 });
 
+// POST NEW ACCOUNT
 router.post("/", async (req, res, next) => {
   try {
     const payload = {
@@ -41,6 +44,7 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+// EDIT/UPDATE ACCOUNT
 router.put("/:id", validateAccountsId, async (req, res, next) => {
   try {
     const payload = {
@@ -61,6 +65,7 @@ router.put("/:id", validateAccountsId, async (req, res, next) => {
   }
 });
 
+//DELETE ACCOUNT
 router.delete("/:id", validateAccountsId, async (req, res, next) => {
   try {
     await db("accounts")
@@ -71,6 +76,48 @@ router.delete("/:id", validateAccountsId, async (req, res, next) => {
     next(err);
   }
 });
+
+// STRETCH
+
+// GET ALL BY DESCENDING BUDGET AMT
+// router.get("/", async (req, res, next) => {
+//   try {
+//     res.json(
+//       await db("accounts")
+//         .orderBy("budget", "desc")
+//         .select()
+//     );
+//   } catch (err) {
+//     next();
+//   }
+// });
+
+// Get 10 accounts by budget, descending values.
+// router.get("/", async (req, res, next) => {
+//   try {
+//     res.json(
+//       await db("accounts")
+//         .orderBy("budget", "desc")
+//         .limit(10)
+//         .select()
+//     );
+//   } catch (err) {
+//     next();
+//   }
+// });
+
+// GET ALL BUDGETS > 5000.00
+// router.get("/", async (req, res, next) => {
+//   try {
+//     res.json(
+//       await db("accounts")
+//         .where("budget", ">", 5000.0)
+//         .select()
+//     );
+//   } catch (err) {
+//     next();
+//   }
+// });
 
 // Middleware
 async function validateAccountsId(req, res, next) {
